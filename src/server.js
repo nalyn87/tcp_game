6dc3f57f6297ffdf756177bf583fe5a7da59140f
nalyn87 <nalyn0807@gmail.com>
2024-06-24 19:10:25 +0900

    ✨ update - 주특기 플러스 강의 3-1

---
+++ src/server.js
@@ -0,0 +1,31 @@
+import net from "net";
+import initServer from "./init/index.js";
+
+const PORT = 5555;
+
+const server = net.createServer((socket) => {
+  console.log(`Client connected from: ${socket.remoteAddress}:${socket.remotePort}`);
+
+  socket.on("data", (data) => {
+    console.log(data);
+  });
+
+  socket.on("end", () => {
+    console.log("Client disconnected");
+  });
+
+  socket.on("error", (err) => {
+    console.error("Socket error:", err);
+  });
+});
+
+initServer().then(() => {
+    server.listen(PORT, () => {
+      console.log(`Echo server listening on port ${PORT}`);
+      console.log(server.address());
+    });
+}).catch((err) => {
+    console.error(err);
+    process.exit(1);
+})
+
