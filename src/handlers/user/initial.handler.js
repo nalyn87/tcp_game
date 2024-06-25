ae7176f950da657987f90d1060544baa3294cb41
nalyn87 <nalyn0807@gmail.com>
2024-06-25 11:41:29 +0900

    ✨ update - 주특기 플러스 강의 3-6 세션, 핸들러 추가

---
+++ src/handlers/user/initial.handler.js
@@ -0,0 +1,20 @@
+import { HANDLER_IDS, RESPONSE_SUCCESS_CODE } from "../../constants/handlerIds.js";
+import { addUser } from "../../sessions/user.session.js";
+import { createResponse } from "../../utils/response/createReponse.js";
+
+const initialHandler = async ({ socket, userId, payload }) => {
+  const { deviceId } = payload;
+
+  addUser(socket, deviceId);
+
+  const initialResponse = createResponse(
+    HANDLER_IDS.INITIAL,
+    RESPONSE_SUCCESS_CODE,
+    {userId : deviceId},
+    deviceId,
+  )
+
+  socket.write(initialResponse);
+};
+
+export default initialHandler;
--- src/utils/parser/packetParser.js
+++ src/utils/parser/packetParser.js
@@ -1,22 +1,55 @@
-import { getProtoMessages } from "../../init/loadProtos.js"
+import { config } from '../../config/config.js';
+import { getProtoTypeNameByHandlerId } from '../../handlers/index.js';
+import { getProtoMessages } from '../../init/loadProtos.js';
 
 export const packetParser = (data) => {
-    const protoMessages = getProtoMessages();
+  const protoMessages = getProtoMessages();
 
-    const Packet = protoMessages.common.Packet;
-    let packet;
-    try {
-        packet = Packet.decode(data);
-    } catch (err) {
-        console.error(err);
-    }
+  const Packet = protoMessages.common.Packet;
+  let packet;
+  try {
+    packet = Packet.decode(data);
+  } catch (err) {
+    console.error(err);
+  }
 
-    const handlerId = packet.handlerId;
-    const userId = packet.userId;
-    const clientVersion = packet.clientVersion;
-    const payload = packet.payload;
-    const sequence = packet.sequence;
+  const handlerId = packet.handlerId;
+  const userId = packet.userId;
+  const clientVersion = packet.clientVersion;
+  const sequence = packet.sequence;
 
-    console.log(`clientVersion: ${clientVersion}`);
-    return {handlerId, userId, payload, sequence};
-}
+  if (clientVersion !== config.client.version) {
+    console.error('클라이언트 버전이 일치하지 않습니다');
+  }
+
+  const protoTypeName = getProtoTypeNameByHandlerId(handlerId);
+  if (!protoTypeName) {
+    console.error('알 수 없는 핸들러 ID: ', handlerId);
+  }
+
+  const [namespace, typeName] = protoTypeName.split('.');
+  const PayloadType = protoMessages[namespace][typeName];
+  let payload;
+
+  try {
+    payload = PayloadType.decode(packet.payload);
+  } catch (err) {
+    console.error(err);
+  }
+
+  const errorMessage = PayloadType.verify(payload);
+  if (errorMessage) {
+    console.error(`패킷 구조가 일치하지 않습니다: ${errorMessage}`);
+  }
+
+  // 필수 필드가 누락된 경우
+  const expectedFields = Object.keys(PayloadType.fields);
+  const actualFields = Object.keys(payload);
+  const missingFields = expectedFields.filter((field) => !actualFields.includes(field));
+
+  if (missingFields.length > 0) {
+    console.error(`필수 필드가 누락되었습니다: ${missingFields.join(', ')}`);
+  }
+
+  return { handlerId, userId, payload, sequence };
+};
