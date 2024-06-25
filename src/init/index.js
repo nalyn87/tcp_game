f3da033d0892ade02aad2e8234631fb5f6204b63
nalyn87 <nalyn0807@gmail.com>
2024-06-25 15:38:30 +0900

    ✨ update - 주특기 플러스 강의 3-8 DB 연동

--- src/init/index.js
+++ src/init/index.js
@@ -1,3 +1,5 @@
+import pools from "../db/database.js";
+import { testAllConnections } from "../utils/db/testConnection.js";
 import { loadGameAssets } from "./assets.js";
 import { loadProtos } from "./loadProtos.js";
 
@@ -5,6 +7,7 @@ const initServer = async () => {
   try {
     await loadGameAssets();
     await loadProtos();
+    await testAllConnections(pools);
   } catch (err) {
     console.error(err);
     process.exit(1);
