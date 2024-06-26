f787a2047fcc2022d005b075820f1aa42b6360c2
nalyn87 <nalyn0807@gmail.com>
2024-06-26 15:43:35 +0900

    ✨ update - 주특기 플러스 강의 3-14 핑 메서드 추가

---
+++ src/utils/notification/game.notification.js
@@ -0,0 +1,26 @@
+import { config } from '../../config/config.js';
+import { PACKET_TYPE } from '../../constants/header.js';
+import { getProtoMessages } from '../../init/loadProtos.js';
+
+const makeNotification = (message, type) => {
+  const packetLength = Buffer.alloc(config.packet.totalLength);
+  packetLength.writeUInt32BE(
+    message.length + config.packet.totalLength + config.packet.typeLength,
+    0,
+  );
+
+  const packetType = Buffer.alloc(config.packet.typeLength);
+  packetType.writeUInt8(type, 0);
+
+  return Buffer.concat([packetLength, packetType]);
+};
+
+export const createPingPacket = (timestamp) => {
+  const protoMessages = getProtoMessages();
+  const ping = protoMessages.common.ping;
+
+  const payload = { timestamp };
+  const message = ping.create(payload);
+  const pingPacket = ping.encode(message).finish();
+  return makeNotification(pingPacket, PACKET_TYPE.PING)
+};
